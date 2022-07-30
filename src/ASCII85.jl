625a75d05c80a6e6d719b16505bb4226d4bf7a29
pst-lz <p.stehring@gmail.com>
2022-07-30 20:46:35 +0200

    a

--- src/ASCII85.jl
+++ src/ASCII85.jl
@@ -9,7 +9,7 @@ function ascii85enc!(in::IO, out::IO)
     seekstart(out)
     write(out, "<~")
     inarr = Array{UInt8}(undef, 0)
-    inarr = read(in, inarr)
+    inarr = read(in)
     seg :: UInt32 = 0
     segenc = zeros(UInt8, 5)
     numseg = length(inarr) ÷ 4
