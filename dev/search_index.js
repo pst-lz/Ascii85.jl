3ca91456742403fd93aa286b40b39bff43abc89c
Documenter.jl <documenter@juliadocs.github.io>
2022-08-03 19:15:39 +0000

    build based on d5613a6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Ascii85.jl","page":"Ascii85.jl","title":"Ascii85.jl","text":"","category":"section"},{"location":"","page":"Ascii85.jl","title":"Ascii85.jl","text":"Documentation for Ascii85.jl","category":"page"}]
+}
